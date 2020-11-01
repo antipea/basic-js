36e10661ac949be774123bd1fb93a498da299882
antipea <71250351+antipea@users.noreply.github.com>
2020-11-01 12:21:05 +0300

    feat: implement createDreamTeam()

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members) === false) {
+    return false;
+  }
+  let  newName = '';
+  for ( let i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string") {
+      newName = `${newName}${members[i].trim().slice(0,1).toUpperCase()}`;
+    }
+  }
+
+  return newName.split('').sort().join('');
 };
