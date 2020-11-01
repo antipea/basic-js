c6083dd179d267c80872372f5ecd0f2bd4f61d6f
antipea <71250351+antipea@users.noreply.github.com>
2020-11-01 12:22:44 +0300

    feat: implement repeater()

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let newStr = '';
+  let addition = '';
+
+  str = (str === null) ? 'null' : str.toString();
+  if (options !== undefined) {
+    if (options.addition === null) {
+      addition = 'null';
+    } else {
+      addition = (options.addition === undefined ) ? '' : options.addition.toString();
+    }
+    let counter = (options.repeatTimes !== undefined) ? options.repeatTimes : 1;
+    let separator = (options.separator !== undefined) ? options.separator : '+';
+    let additionSeparator = (options.additionSeparator === undefined) ? '' : options.additionSeparator;
+    let additionRepeatTimes = (options.additionRepeatTimes !== undefined) ? options.additionRepeatTimes : 1;
+    for (let i = 1; i <= counter; i++) {
+      newStr = `${newStr}${str}${addition}`;
+      if (additionRepeatTimes > 1) {
+        for (let j = 1; j < additionRepeatTimes; j++) {
+          newStr = `${newStr}${additionSeparator}${addition}`;
+        }
+      }
+      if (i !== counter) {
+        newStr = `${newStr}${separator}`;
+      }
+    }
+  } else {
+    newStr = `${str}|+`;
+  }
+
+  return newStr;
 };
   
